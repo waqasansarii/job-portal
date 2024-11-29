e47cac6fce033ebbc39219fd91652b8ed749b197
smashcodedev <65023462+waqasansarii@users.noreply.github.com>
2024-11-29 17:10:01 +0500

    updated setting

--- job-portal/main/settings.py
+++ job-portal/main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-z-5sk+!tgqqvew^!=$0#4x7z@ays^l@2(ksny43im_aahms6=a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
