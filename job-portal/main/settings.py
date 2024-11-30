ef96d9f057fdaea3c3794d51da516fd422e24c6e
smashcodedev <65023462+waqasansarii@users.noreply.github.com>
2024-11-30 22:28:09 +0500

    added choreo file

--- job-portal/main/settings.py
+++ job-portal/main/settings.py
@@ -127,7 +127,9 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS =os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
